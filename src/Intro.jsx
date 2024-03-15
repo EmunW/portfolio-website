21601eda78ce56a642ad439b9fb9906f6822add8
EmunW <edmundpunch5@gmail.com>
2024-03-15 00:13:30 -0400

    Fixed logo url

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -6,12 +6,12 @@ export default function Intro() {
           <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-teal-300/50 dark:bg-white/5 px-6 py-16 ring-1 ring-white/10 md:rounded-3xl md:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
             <img
               className="w-1/2 dark:hidden"
-              src="/./photos/EWLogo.png"
+              src="src/photos/EWLogo.png"
               alt="My logo"
             ></img>
             <img
               className="w-1/2 hidden dark:block"
-              src="/./photos/EWLogoWhite.png"
+              src="src/photos/EWLogoWhite.png"
               alt="My logo"
             ></img>
             <div className="w-full flex-auto">
