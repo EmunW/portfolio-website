96f276740ff25fa3cd02ca117e0f434e0f022893
EmunW <edmundpunch5@gmail.com>
2024-03-26 17:18:39 -0400

    Busy

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -4,7 +4,7 @@ export default function TechStack() {
       <div className="flex flex-col gap-10 mx-auto sm:px-6 lg:px-8 bg-teal-300/50 dark:bg-white/5 px-6 py-16 ring-1 ring-white/10 md:rounded-3xl">
         <div className="flex flex-col items-center gap-3">
           <h2 className="text-3xl font-bold tracking-tight dark:text-white md:text-4xl">
-            Here is my main tech stack
+            Here is my main tech stack (Busy ATM will finish section tomorrow)
           </h2>
         </div>
 
